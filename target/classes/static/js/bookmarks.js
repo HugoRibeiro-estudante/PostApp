b56e072aeb828c2a77a4dff9ff366ecd243abcb8
marianafvmelo <marymelomafvm@gmail.com>
2022-12-10 17:36:11 -0300

    Início do desenvolvimento dos favoritos

---
+++ target/classes/static/js/bookmarks.js
@@ -0,0 +1,9 @@
+let boomarks = [];
+
+const openBoxBookmark = () => {
+  document.getElementById("boxBookmark").classList.remove("hidden");
+};
+
+const closeBoxBookmark = () => {
+  document.getElementById("boxBookmark").classList.add("hidden");
+};
