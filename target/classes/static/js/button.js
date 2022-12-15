0ec7b0eb23fe8eba29ab099403f779e50188f48a
marianafvmelo <marymelomafvm@gmail.com>
2022-12-14 22:29:39 -0300

    Alterações na página de criar post

--- target/classes/static/js/button.js
+++ target/classes/static/js/button.js
@@ -22,37 +22,14 @@ const closePostOptions = () => {
   clickStatus--;
 };
 
-const bold = () => {
-  const span = document.createElement("span");
-  const textarea = document.getElementById("elem");
-  console.log(textarea);
-  textarea.appendChild(span);
-  span.classList.toggle("bold");
-};
 
-let editableDiv = document.createElement("div");
-// Adding text to that created element
-let value = localStorage.getItem("text");
-let text;
-
-if (value == null) {
-  text = document.createTextNode("Click on it to edit this Editable Div");
-} else {
-  text = document.createTextNode(value);
-}
-editableDiv.appendChild(text);
-editableDiv.setAttribute("id", "elem");
-editableDiv.setAttribute("class", "elem");
-editableDiv.setAttribute("contentEditable", "true");
-editableDiv.setAttribute(
-  "style",
-  "font-size:3rem;border:3px solid;width:100%;height:800px;"
-);
-
-// Access the main divTxtarea
-
-let divTxtarea = document.querySelector(".divTxtarea");
-
-// Inserting the element with id = first
-
-divTxtarea.insertBefore(editableDiv, first);
+tinymce.init({
+  selector: 'textarea#tiny',
+plugins: [
+      'a11ychecker', 'advlist', 'advcode', 'advtable', 'autolink', 'checklist', 'export',
+      'lists', 'link', 'image', 'charmap', 'preview', 'anchor', 'searchreplace', 'visualblocks',
+      'powerpaste', 'fullscreen', 'formatpainter', 'insertdatetime', 'media', 'table', 'help', 'wordcount'
+  ],
+  toolbar: 'undo redo | a11ycheck casechange blocks | bold italic backcolor | alignleft aligncenter alignright alignjustify |' +
+      'bullist numlist checklist outdent indent | removeformat | code table help'
+})
